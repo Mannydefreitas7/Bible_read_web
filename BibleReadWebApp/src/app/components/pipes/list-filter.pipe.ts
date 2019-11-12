97bf930d74a91628d057db5a106a0e056223ca68
De Freitas, Manuel <mdefreitas@bethel.jw.org>
2019-11-12 09:37:19 -0500

    start over

--- BibleReadWebApp/src/app/components/pipes/list-filter.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-@Pipe({
-  name: 'filter'
-})
-export class ListFilterPipe implements PipeTransform {
-	transform(value: any[], searchText: string): any[] {
-		return value.filter((x:any) => x.fname.trim().toLowerCase().startsWith(searchText.trim().toLowerCase()) || x.lname.trim().toLowerCase().startsWith(searchText.trim().toLowerCase()))
-	}
-}
